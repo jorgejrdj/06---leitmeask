4cf93a3a208f303a4558cc6fe88260288824f7de
Jorge de Souza <jorgejrdj1@gmail.com>
2021-06-23 17:43:10 -0300

    NewRoom + buttom estilizados

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,11 +1,10 @@
 import illustrationIMG from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss'
 import { Button } from '../components/Button';
 
-export function Home() {
+export function NewRoom() {
   return (
     <div id="page-auth">
       <aside>
@@ -16,21 +15,21 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="LetmeAsk" />
-          <button className="create-room">
-            <img src={googleIconImg} alt="Logo do Google" />
-            Crie sua sala com o Google
-          </button>
-          <div className="separator"> ou entre em uma sala</div>
+          <h2>Criar uma nova sala</h2>
+
           <form>
             <input
               type="text"
-              placeholder="Digite o código da sala"
+              placeholder="Nome da sala"
 
             />
             <Button type="submit">
-              Entrar na sala
+              Criar sala
             </Button>
           </form>
+          <p>
+            Quer entrar em uma sala que já existe? <a href="#">Clique aqui</a>
+          </p>
         </div>
       </main>
     </div>
