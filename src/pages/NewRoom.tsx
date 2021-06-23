65bd5dc04f492865e357b4aacbb13c447e4b5130
Jorge de Souza <jorgejrdj1@gmail.com>
2021-06-23 17:59:20 -0300

    navegação com react-route-dom

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import illustrationIMG from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 
@@ -28,7 +30,7 @@ export function NewRoom() {
             </Button>
           </form>
           <p>
-            Quer entrar em uma sala que já existe? <a href="#">Clique aqui</a>
+            Quer entrar em uma sala que já existe? <Link to="/">Clique aqui</Link>
           </p>
         </div>
       </main>
